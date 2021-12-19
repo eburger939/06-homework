b286a660e3d208f29ad98ef729635b590fb98739
Emily Burger <eburger939@gmail.com>
2021-12-19 17:30:23 -0600

    did a for loop with temperature, created an array to try and get each value to display in each box.  Not working still

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var cityList = $('.city-list');
 var currWeather = $('.current-w');
 var apiKey = "1cfb34a53bf8b5e073240d3987394c4d";
 var cards5 = $(".cards");
-var day5 = [];
+var tempArray = [];
 var space = $(".space");
 
 
@@ -140,38 +140,49 @@ function endApi(lat, lon) {
     
 
 for (var i=0; i<5; i++) {
-    var forecast = data.daily[i]
-    console.log(forecast)
-
-
-
-    var iconCode = forecast.weather[0].icon;
-    console.log(iconCode)   
-    var temp5 = forecast.temp.day;
+    var temp5 = data.daily[i].temp.day
     console.log(temp5)
-    var humidity5 = forecast.humidity;
-    var windSpeed5 = forecast.wind_speed;
-}  
-    var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
-        $('#wicon').attr('src', iconurl)
-        console.log(iconurl)
-        
+    
+    tempArray.push(temp5)
+    console.log(tempArray)
+}
+
+    tempArray.each(function(value) {
         var temper = document.createElement('p');
-        temper.textContent = 'Temperature (F): ' +temp5;
+        temper.textContent = 'Temperature (F): ' +value;
+        console.log(temper)
         cards5.append(temper)
+    })
+
+
+
+    // var iconCode = forecast.weather[0].icon;
+    // console.log(iconCode)   
+    // var temp5 = forecast.temp.day;
+    // console.log(temp5)
+    // var humidity5 = forecast.humidity;
+    // var windSpeed5 = forecast.wind_speed;
+
+    // var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    //     $('#wicon').attr('src', iconurl)
+    //     console.log(iconurl)
+        
+    //     var temper = document.createElement('p');
+    //     temper.textContent = 'Temperature (F): ' +temp5;
+    //     cards5.append(temper)
     
         
-        var humi = document.createElement('p');
-        humi.textContent = 'Humidity: ' +humidity5;
-        cards5.append(humi)
-        console.log(humi);
+    //     var humi = document.createElement('p');
+    //     humi.textContent = 'Humidity: ' +humidity5;
+    //     cards5.append(humi)
+    //     console.log(humi);
 
 
         
-        var windy = document.createElement('p');
-        windy.textContent = 'Wind speed (mph): ' +windSpeed5;
-        cards5.append(windy)
-        console.log(windy);
+    //     var windy = document.createElement('p');
+    //     windy.textContent = 'Wind speed (mph): ' +windSpeed5;
+    //     cards5.append(windy)
+    //     console.log(windy);
 
 
 
